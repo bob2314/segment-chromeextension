358e182143a91c43592935af65af7140a8f34bca
Bob Brennan <robert.brennan@foxtv.com>
2020-10-08 20:55:47 -0700

    Add Julias domain updates

--- background.js
+++ background.js
@@ -46,69 +46,52 @@ chrome.extension.onConnect.addListener((port) => {
 
 chrome.webRequest.onBeforeRequest.addListener(
 	(details) => {
-    // Fox segment domain update:
-	// Request URL: https://api.dev-nova.foxtv.com/v1/t
-	const base_uri = 'nova.foxtv'
-    if (
-      details.url.startsWith("https://api.segment.io/v1") ||
-		  details.url.includes("nova.foxtv") ||
-      details.url.includes(base_uri)
-    ) {
-      var postedString = String.fromCharCode.apply(
-        null,
-        new Uint8Array(details.requestBody.raw[0].bytes)
-      );
+		if (details.url.startsWith('https://api.segment.io/v1') || details.url.startsWith('https://api.dev-nova.fox') || details.url.startsWith('http://api.dev-nova.fox') || details.url.startsWith('https://api.nova.fox')) {
+			var postedString = String.fromCharCode.apply(null,new Uint8Array(details.requestBody.raw[0].bytes));
 
-      var rawEvent = JSON.parse(postedString);
+			var rawEvent = JSON.parse(postedString);
 
-      var today = new Date();
+			var today = new Date();
 
-      var h = zeroPad(today.getHours());
-      var m = zeroPad(today.getMinutes());
-      var s = zeroPad(today.getSeconds());
+			var h = zeroPad(today.getHours());
+			var m = zeroPad(today.getMinutes());
+			var s = zeroPad(today.getSeconds());
 
-      var event = {
-        eventName: rawEvent.event,
-        raw: postedString,
-        trackedTime: h + ":" + m + ":" + s,
-      };
+			var event = {
+				eventName: rawEvent.event,
+				raw: postedString,
+				trackedTime: h + ':' + m + ':' + s,
+			};
 
-      chrome.tabs.query(
-        {
-          active: true,
-          currentWindow: true,
-        },
-        (tabs) => {
-          var tab = tabs[0];
+			chrome.tabs.query({
+				active: true,
+				currentWindow: true
+			}, (tabs) => {
+				var tab = tabs[0];
 
-          event.hostName = tab.url;
-          event.tabId = tab.id;
+				event.hostName = tab.url;
+				event.tabId = tab.id;
 
-          /**
-           * Check or valid domains
-           * 1) api.segment.io/v1/t
-           * 2) nova.foxtv.com/v1/1
-           * 3) nova.foxtv.com/v1/p
-           */
-          if (details.url == "https://api.segment.io/v1/t" || details.url.includes('nova.foxtv') && details.url.includes('v1/t')) {
-            event.type = "track";
+				if ((details.url.startsWith('https://api.segment.io/v1') || details.url.startsWith('https://api.dev-nova.fox') || details.url.startsWith('http://api.dev-nova.fox') || details.url.startsWith('https://api.nova.fox')) && details.url.includes('/v1/t')) {
+					event.type = 'track';
 
-            trackedEvents.unshift(event);
-          } else if (details.url == "https://api.segment.io/v1/1" || details.url.includes('nova.foxtv') && details.url.includes('v1/1')) {
-            event.eventName = "Identify";
-            event.type = "identify";
+					trackedEvents.unshift(event);
+				}
+				else if ((details.url.startsWith('https://api.segment.io/v1') || details.url.startsWith('https://api.dev-nova.fox') || details.url.startsWith('http://api.dev-nova.fox') || details.url.startsWith('https://api.nova.fox')) && details.url.includes('/v1/i')) {
+					event.eventName = 'Identify';
+					event.type = 'identify';
 
-            trackedEvents.unshift(event);
-        } else if (details.url == "https://api.segment.io/v1/p" || details.url.includes('nova.foxtv') && details.url.includes('v1/p')) {
-            event.eventName = "Page loaded";
-            event.type = "pageLoad";
+					trackedEvents.unshift(event);
+				}
+				else if ((details.url.startsWith('https://api.segment.io/v1') || details.url.startsWith('https://api.dev-nova.fox') || details.url.startsWith('http://api.dev-nova.fox') || details.url.startsWith('https://api.nova.fox')) && details.url.includes('/v1/p')) {
+					event.eventName = 'Page loaded';
+					event.type = 'pageLoad';
 
-            trackedEvents.unshift(event);
-          }
-        }
-      );
-    }
-  },
+					trackedEvents.unshift(event);
+				}
+			});
+		}
+	},
 	{
 	urls: [
 		"https://*/*",
